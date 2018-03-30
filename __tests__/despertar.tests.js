73af8ecd480b1b622c94cdc78bddc0dd6f0645e0
mariomelo <mariofdemelo@gmail.com>
2018-03-29 23:41:31 -0300

    Corrige o teste do despertar

--- __tests__/despertar.tests.js
+++ __tests__/despertar.tests.js
@@ -8,12 +8,12 @@ describe('Ao despertar, o desenvolvedor', () => {
   it('deve acordar bem disposto caso levante rapidamente', () => {
     let result = despertar.getOutcome(INITIAL_HUMOR, WAKEUP)
 
-    expect(result).toEqual(2)
+    expect(result.result).toEqual(2)
   })
 
   it('deve ficar de mau-humor caso ligue o soneca', () => {
     let result = despertar.getOutcome(INITIAL_HUMOR, SNOOZE)
 
-    expect(result).toEqual(-2)
+    expect(result.result).toEqual(-2)
   })
 })
